66fa5880cab2d0beefe7f9db3ed4982a46b4d87a
romoney5 <andysin417@gmail.com>
2023-08-15 12:23:52 -0400

    Add de help but not wok

--- yt-dlpGUI.py
+++ yt-dlpGUI.py
@@ -1,8 +1,13 @@
+import logging
 import random
+import threading
+import traceback
 
 import yt_dlp.utils
-from kivy import Config
+from kivy import Config, app
+from kivy.animation import Animation
 from kivy.app import App
+from kivy.clock import mainthread
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -22,37 +27,92 @@ class ytdlpgui(App):
         self.url_input = TextInput(hint_text='URL/Arguments', pos=(10, 70), size_hint=(.4, .05))
         download_button = Button(text='Download video', on_release=self.download, pos=(10, 20), size_hint=(.4, .08))
         self.progress_bar = ProgressBar(max=100, value=0, pos=(10, 0), size_hint=(1, .05))
-        # YoutubeDL().get
-        self.consolelog = TextInput(hint_text='URL/Arguments', size_hint=(.8, .8),pos_hint={'right': .98, 'top': .98}, disabled=True)
-
+        self.consolelog = TextInput(text=YoutubeDL.__doc__, size_hint=(.8, .8),pos_hint={'right': .98, 'top': .98})
+        help(YoutubeDL)
+        # self.setup_logger()
         layout.add_widget(self.url_input)
         layout.add_widget(download_button)
         layout.add_widget(self.progress_bar)
         layout.add_widget(self.consolelog)
 
         return layout
+    # def setup_logger(self):
+    #     log_handler = self.LogHandler(self.consolelog)
+    #     ydl_logger = logging.getLogger('yt_dlp')
+    #     ydl_logger.addHandler(log_handler)
+    #     ydl_logger.setLevel(logging.DEBUG)
     # def build(self):
     #     sm = ScreenManager()
     #     sm.add_widget(MainScreen(name='main'))
     #     sm.add_widget(SecondScreen(name='second'))
     #     return sm
     def download(self,instance=None):
-        self.progress_bar.value = random.randint(1,100)
+        Animation(value=0,duration=.3).start(self.progress_bar)
         text_input = self.url_input # Access the TextInput widget by its ID
         url = text_input.text
         print("Now downloading ", url)
-        with yt_dlp.YoutubeDL() as ydl:
-            try:
-                info_dict = ydl.extract_info(url, download=False)
-                if info_dict:
-                    print("The URL is valid ", url)
-                    YoutubeDL().download([url])
-                else:
+        self.consolelog.text=''
+        def dlthread():
+            ydl_opts = {
+                'logger': self.LogHandler(self.consolelog),
+                'progress_hooks': [self.progress_hook],
+                # 'quiet':True,
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                try:
+                    info_dict = ydl.extract_info(url, download=False)
+                    if info_dict:
+                        print("The URL is valid ", url)
+                        ydl.download([url])
+                    else:
+                        print("Invalid URL ", url)
+                        return False
+                except Exception as e:
                     print("Invalid URL ", url)
+                    self.addtolog(f"An error occured while downloading {url}, lemme show you:\n{traceback.format_exc()}")
                     return False
-            except Exception:
-                print("Invalid URL ", url)
-                return False
+        threading.Thread(target=dlthread).start()
+
+# ℹ️ See "progress_hooks" in help(yt_dlp.YoutubeDL)
+    @mainthread
+    def progress_hook(self, d):
+        # app.stopTouchApp()
+        if d['status'] == 'downloading':
+            # progress_text = f"Downloading {d['filename']}: {d['_percent_str']} complete\n"
+            # self.addtolog(progress_text)
+            Animation(value=int(round(float(d['_percent_str'].rstrip("%")))),duration=.3).start(self.progress_bar)
+        elif d['status'] == 'finished':
+            completed_text = f"The download of \"{d['filename']}\" has been completed\n"
+            self.addtolog(completed_text)
+    class LogHandler(logging.Handler):
+        def __init__(self, label):
+            super().__init__()
+            self.label = label
+        @mainthread
+        def emit(self, record):
+            msg = self.format(record)
+            self.label.text += f"{msg}\n"
+        @mainthread
+        def debug(self, msg):
+            # For compatibility with youtube-dl, both debug and info are passed into debug
+            # You can distinguish them by the prefix '[debug] '
+            if msg.startswith('[debug] '):
+                pass
+            else:
+                self.label.text += f"{msg}\n"
+        @mainthread
+        def info(self, msg):
+            self.label.text += f"{msg}\n"
+        @mainthread
+        def warning(self, msg):
+            self.label.text += f"{msg}\n"
+        @mainthread
+        def error(self, msg):
+            self.label.text += f"{msg}\n"
+            print("For the bois that are looking at the log: "+msg)
+    @mainthread
+    def addtolog(self,msg):
+        self.consolelog.text += f"{msg}\n"
 # class MainScreen(Screen):
 #     pass
 # class SecondScreen(Screen):
